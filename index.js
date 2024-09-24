5ba31ab48ad21e3fd9abcf7ba00eca5b19ab9568
Monowara Moni <monowaramonielination@gmail.com>
2024-09-24 22:22:53 +0600

    index rename

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// blog btm link 
+
 document.getElementById('blog-btn').addEventListener('click', function () {
      window.location.href = "./blog.html ";
 
 
 })
+
+// donation input validation
+
+const inputDonation = document.getElementById(id)
