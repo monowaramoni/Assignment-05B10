a35d38a7ce2287f7525700e3e716b61543077d08
Monowara Moni <monowaramonielination@gmail.com>
2024-09-25 17:03:27 +0600

    tab switich

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 // blog btm link 
 
 document.getElementById('blog-btn').addEventListener('click', function () {
-     window.location.href = "./blog.html ";
+    window.location.href = "./blog.html ";
 
 
 })
 
- const donationAmount =document.getElementById('donate-Btn').addEventListener('click', function (event){
-     event.preventDefault;
-     const inputDonation = parseFloat(document.getElementById('input-donation').value);
-    //  const currentAmountElements = document.getElementById('current-amount').innerText;
+const donationAmount = document.getElementById('donate-Btn').addEventListener('click', function (event) {
+    event.preventDefault;
+    const inputDonation = parseFloat(document.getElementById('input-donation').value);
+    //  const currentAmountElement = document.getElementById('current-amount');
+    //  currentAmountElement.innerText = current-amount.toFixed(2);
     //  const donationAmount = inputDonation + currentAmount;
-     console.log(inputDonation);
- })
- 
-// function for all donate now form
+    console.log();
 
-//  const donationAmount = document.getElementById('donate-Btn').addEventListener('click', function (event){
-//      event.preventDefault;
-//      const inputDonation = parseFloat(document.getElementById('donate-Btn').value);
-//      console.log(inputDonation);
-//  })
+})
+
+//  tab switch Donation to history
+
+const historyTab = document.getElementById("history-tab");
+historyTab.addEventListener('click', function () {
+    historyTab.classList.add("bg-btn-colors");
+
+    const donationTab = document.getElementById("donation-tab");
+    donationTab.classList.remove ("bg-btn-colors");
+
+    document.getElementById("main-section").classList.add('hidden');
+
+})
+
